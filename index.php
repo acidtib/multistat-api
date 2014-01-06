aab358ef9715f725c46b047207b9519b32d1ca06
Dan Vera <dan@yovu.co>
2014-01-06 18:26:38 -0500

    landing page

--- index.php
+++ index.php
@@ -1,1 +1,172 @@
-Hi
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>MultiStat</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+  	<!-- Responsive Skeleton -->
+	<link rel="stylesheet" href="assets/stylesheets/skeleton.css">
+	<!-- Base Style -->
+	<link rel="stylesheet" href="assets/stylesheets/base.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="assets/stylesheets/layout.css">
+	<!-- Font Awesome Icons -->
+	<link rel="stylesheet" href="assets/stylesheets/font-awesome.css">
+	<!-- Sliders Style -->
+	<link href="assets/stylesheets/jquery.bxslider.css" rel="stylesheet" />
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>    
+</head>
+<body>
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+		<header class="iphone"><!-- Header -->
+			<div class="container">
+				<div class="logo eleven columns"><!-- Logo -->
+					<img src="assets/images/ios7-app.png" style="width: 60px; height: 60px; -webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px;" />
+					<h1>MultiStat</h1>
+				</div><!-- End Logo -->
+				<div class="intro-text eleven columns"><!-- Intro -->
+					<h1>Take Multipool on the road</h1>
+					
+					<div style="overflow: hidden;">
+						<a href="#" style="float: left;">
+							<div class="appstore-button border-5">
+								<i class="fa fa-mobile-phone fa-4x" style="padding-top: 7px; top: 4px; font-size: 45px;"></i><span>Available on the <br/><strong>App Store</strong></span>
+							</div>
+						</a>
+
+						<a href="#" style="float: left; margin-left: 20px;">
+							<div class="appstore-button border-5">
+							 	<i class="fa fa-android fa-4x" style="padding-top: 7px; top: 4px; font-size: 45px; margin: 0px 0px 0px -6px;"></i><span style="margin-left: 2px;">Get it on the <br><strong>Play Store</strong></span>
+							</div>
+						</a>
+					</div>
+				</div><!-- End Intro -->	
+			</div>
+		</header><!-- End Header -->
+		<section id="info"><!-- Info -->
+			<div class="container">
+				<div class="left eleven columns"><!-- Left -->
+					<div class="info-box first-box ten columns"><!-- White Hover Box -->
+						<div class="icon two columns">
+							<i class="fa fa-cogs fa-4x"></i>
+						</div>
+						<div class="info seven columns padding-10">
+							<h3>Keep Tabs On Your Workers</h3>
+							<p>Fruitcake lemon drops macaroon tiramisu candy bonbon tootsie roll gingerbread cake. Topping soufflé cheesecake ice cream. Topping powder jelly beans toffee.</p>
+						</div>
+						<div class="arrow border-5"></div>
+					</div>
+					<div class="info-box second-box ten columns">
+						<div class="icon two columns">
+							<i class="fa fa-bitcoin fa-4x"></i>
+						</div>
+						<div class="info seven columns padding-10">
+							<h3>Balance</h3>
+							<p>Fruitcake lemon drops macaroon tiramisu candy bonbon tootsie roll gingerbread cake. Topping soufflé cheesecake ice cream. Topping powder jelly beans toffee.</p>
+						</div>
+						<div class="arrow border-5"></div>
+					</div>
+					<div class="info-box third-box ten columns">
+						<div class="icon two columns">
+							<i class="fa fa-money fa-4x"></i>
+						</div>
+						<div class="info seven columns padding-10">
+							<h3>Transactions</h3>
+							<p>Fruitcake lemon drops macaroon tiramisu candy bonbon tootsie roll gingerbread cake. Topping soufflé cheesecake ice cream. Topping powder jelly beans toffee.</p>
+						</div>
+						<div class="arrow border-5"></div>
+					</div>
+				</div><!-- End Left -->
+				<div class="right five columns"><!-- Right -->
+					<div class="iphone-image">
+						<div class="slider-app">
+							<ul class="bxslider appslider">
+							    <li><img src="assets/images/app/workers.png"></li>
+							    <li><img src="assets/images/app/balance.png"></li>
+							    <li><img src="assets/images/app/transactions.png"></li>
+							</ul>
+						</div>
+					</div>
+				</div><!-- End Right -->
+			</div>
+		</section><!-- End Info -->
+	
+	<section style="display: none;" id="testimonials"><!-- Testimonials -->
+		<div class="container">
+				<ul class="bxslider testimonialsslider">
+					<li>
+					<div class="image three columns">
+						<img src="http://placehold.it/120x120">
+					</div>
+					<div class="thirteen columns">
+						<blockquote>"Applicake unerdwear marshmallow jelly beans cookie biscuit unerdwear chupa chups sweet roll. Chocolate cake topping chocolate bar tart topping sweet roll. Powder chupa chups icing biscuit."<cite>Johnny Does</cite></blockquote> 
+					</div></li>
+					<li>
+					<div class="image three columns">
+						<img src="http://placehold.it/120x120">
+					</div>
+					<div class="thirteen columns">
+						<blockquote>"Applicake unerdwear marshmallow jelly beans cookie biscuit unerdwear chupa chups sweet roll. Chocolate cake topping chocolate bar tart topping sweet roll. Powder chupa chups icing biscuit."<cite>Do Johnson</cite></blockquote> 
+					</div>
+					</li>
+			</div>
+		</div>
+	</section><!-- End Testimonials -->
+	
+	
+	<footer id="footer"><!-- Footer -->
+		<div class="container">
+			<div class="copyright eleven columns offset-by-one"><!-- Copyright -->
+				<p>MultiStat is handcrafted by <a href="https://twitter.com/drkyro" style="color: #ffffff;">Dan Vera</a></p>
+			</div>
+			<div class="social four columns"><!-- Social Buttons -->
+				<ul>
+					<li class="border-5"><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li class="border-5"><a href="#"><i class="fa fa-twitter"></i></a></li>
+
+				</ul>
+			</div>
+		</div>
+	</footer><!-- End Footer -->
+
+
+	<script type="text/javascript" src='assets/js/jquery.min.js'></script>
+	<script type="text/javascript" src='assets/js/scripts.js'></script>
+	<script type="text/javascript" src='assets/js/jquery.bxslider.js'></script>
+	<script src="assets/js/jquery.slides.js"></script>
+	<script src="assets/js/retina-1.1.0.min.js"></script>
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
