a67b3b77677193df01b03f3937fb4737285f06e9
Dan Vera <dan@yovu.co>
2014-01-19 14:04:58 -0500

    change that api url

--- api/stat.php
+++ api/stat.php
@@ -5,7 +5,8 @@
 	
 	date_default_timezone_set('America/New_York');
 
-	$url = 'https://www.multipool.us/api2.php?api_key='.$_GET['api_key'].'';
+	//$url = 'https://www.multipool.us/api2.php?api_key='.$_GET['api_key'].'';
+	$url = 'http://api.multipool.us/api2.php?api_key='.$_GET['api_key'].'';
 	$cmd = array();
 	$data = array('json' => json_encode($cmd));
  
