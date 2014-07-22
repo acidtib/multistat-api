e765f461f202b061ac1586e5087f8e9929cb94b3
Dan Vera <dan@yovu.co>
2014-07-22 17:40:53 -0400

    somewhat of a structure  mehh

--- index.php
+++ index.php
@@ -1,4 +1,62 @@
 <?php 
 	require 'vendor/autoload.php';
+
+	$app = new \Slim\Slim(array(
+    'debug' => true
+	));
+
+	$app->setName('multistat-api');
+
+	//Routes
+	$app->get('/', 'home');
+
+	$app->group('/api', function () use ($app) {
+
+		$app->group('/v1', function () use ($app) {
+
+			$app->group('/:api', function () use ($app) {
+
+				#currency
+				$app->get('/currency', 'V1:currency');
+
+				#workers
+				$app->get('/workers', 'V1:workers');
+
+				#transactions
+				$app->get('/transactions', 'V1:transactions');
+
+			});
+
+		});
+
+	});
+
+	# lets go
+	$app->run();
+
+
+	function home() {
+		echo "You shall not pass";
+	}
+
+
+	class V1 {
+		
+		#currency
+		function currency($api) {
+			echo "im currency";
+		}
+
+		#workers
+		function workers($api) {
+			echo "im workers";
+		}
+
+		#transactions
+		function transactions($api) {
+			echo "im transactions";
+		}
+
+	}
 	
 ?>
