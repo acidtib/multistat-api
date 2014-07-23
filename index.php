7ccee4ac0cfd895bece6352df8a498b62a8227b6
Dan Vera <dan@yovu.co>
2014-07-22 21:06:00 -0400

    looking good

--- index.php
+++ index.php
@@ -16,13 +16,18 @@
 
 		$app->group('/v1', function () use ($app) {
 
-			$app->get('/:api', 'V1:stat');
+			$app->get('/:api', 'V1:pull');
+
+			$app->group('/:api', function () use ($app) {
+
+				$app->get('/stat', 'V1:stat');
+			});
 
 		});
 
 	});
 
-	//$app->contentType('application/json');
+	$app->contentType('application/json');
 
 	# lets go
 	$app->run();
@@ -31,6 +36,7 @@
 	function home() {
 		echo "You shall not pass";
 
+		#connect
 		$es = new Elasticsearch\Client(
 		  array(
 		    'hosts' => array(
@@ -39,27 +45,28 @@
 		  )
 		);
 
-		$es->index(
-		  array(
-		    'index' => 'multitat',
-		    'type' => 'user',
-		    'id' => '411639a04849a8a9cd2c3da637f313de5e60203abb94ef8a0e69f6127adb91d6',
-		    'body'  => array(
-		      'aur' => array(
-		      	'confirmed_rewards' => '0.0000000000000000',
-		      	'hashrate' => '0',
-		      	'round_shares' => 'false',
-		      	'block_shares' => '0',
-		      	'estimated_rewards' => '0',
-		      	'payout_history' => '0',
-		      	'pool_hashrate' => '6311'
-		      ),
-		      'btc' => '0.0000468070917350',
-		      'cap'    => '0.0654612854123116'
-		    )
-		  )
-		);
+		#index
+		//$params = array();
+    //$params['body']  = array(
+    //		'testField' => 'abc',
+    //		'currency' => array(
+		//      array(
+		//      	'aur' => array(
+		//      		'confirmed_rewards' => '0.000'
+		//      	),
+		//      	'btc' => array(
+		//      		'confirmed_rewards' => '0.000'
+		//      	)
+		//      ),
+		//      array('popularity' => array('order' => 'desc'))
+		//    )
+    //	);
+    //$params['index'] = 'multitat';
+    //$params['type']  = 'user';
+    //$params['id']    = '411639a04849a8a9cd2c3da637f313de5e60203abb94ef8a0e69f6127adb91d6';
+    //$es->index($params);
 
+    #get
 		$doc = $es->get(
 		  array(
 		  	'index' => 'multitat',
@@ -80,6 +87,39 @@
 		#stat
 		public function stat($api) {
 			
+			#connect
+			$es = new Elasticsearch\Client(
+			  array(
+			    'hosts' => array(
+			      '192.241.165.56:9200'
+			    )
+			  )
+			);
+
+			$stat = $es->get(
+			  array(
+			  	'index' => 'multitat',
+	        'type'  => 'user',
+			    'id' => $api
+			  )
+			);
+
+			echo json_encode($stat);
+
+		}
+
+		#pull
+		public function pull($api) {
+
+			#connect
+			$es = new Elasticsearch\Client(
+			  array(
+			    'hosts' => array(
+			      '192.241.165.56:9200'
+			    )
+			  )
+			);
+			
 			$url = 'http://api.multipool.us/api.php?api_key='.$api.'';
 			$cmd = array();
 			$data = array('json' => json_encode($cmd));
@@ -96,9 +136,58 @@
 			$result = file_get_contents($url, false, $context);
 			$result = json_decode($result, true);
 
-			var_dump($result);
+			//var_dump($result);
 
-			echo "im currency";
+			$currency = $result['currency'];
+
+			// check if we have something return
+			if ($currency) {
+
+				foreach ($currency as $currencys => $balance_d) {
+			
+					if ($balance_d['confirmed_rewards'] != 0) {
+						$response['currency'][] = array(
+					 		'currency' => $currencys,
+							'confirmed_rewards' => $balance_d['confirmed_rewards'],
+							'hashrate' => $balance_d['hashrate'],
+							'round_shares' => $balance_d['round_shares'],
+							'block_shares' => $balance_d['block_shares'],
+							'estimated_rewards' => $balance_d['estimated_rewards'],
+							'payout_history' => $balance_d['payout_history'],
+							'pool_hashrate' => $balance_d['pool_hashrate']
+						);
+					}
+
+				}
+
+				#index
+				$params = array();
+		    $params['body']  = $response;
+		    $params['index'] = 'multitat';
+		    $params['type']  = 'user';
+		    $params['id']    = $api;
+		    $es->index($params);
+
+				
+				#get
+				$stat = $es->get(
+				  array(
+				  	'index' => 'multitat',
+		        'type'  => 'user',
+				    'id' => $api
+				  )
+				);
+
+				echo json_encode($stat);
+
+			} else { // panic
+
+				$response['response'] = array(
+					'status' => '500'
+				);
+
+			}
+
 		}
 
 	}
