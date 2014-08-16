d06403ea9a8c4f3ba9a10f6f351cf1aa4f862905
Dan Vera <dan@yovu.co>
2014-08-16 14:57:51 -0400

    clean white spaces

---
+++ from_v1.php
@@ -0,0 +1,154 @@
+<?php
+
+  header('Access-Control-Allow-Origin: *');
+  header('Content-type: application/json;');
+
+  date_default_timezone_set('America/New_York');
+
+  //$url = 'https://www.multipool.us/api2.php?api_key='.$_GET['api_key'].'';
+  $url = 'http://api.multipool.us/api.php?api_key='.$_GET['api_key'].'';
+  $cmd = array();
+  $data = array('json' => json_encode($cmd));
+
+  $options = array(
+      'http' => array(
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($data),
+      ),
+  );
+
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  $result = json_decode($result, true);
+
+  $currency = $result['currency'];
+
+  $balances = $result['currency'];
+
+  $workers = $result['workers'];
+
+
+  //currently mining
+
+  function is_it_sha($btc, $frc, $ppc, $trc, $zet) {
+
+    if ($btc != 0) {
+      $sha = 'BTC';
+    } elseif ($frc != 0) {
+      $sha = 'FRC';
+    } elseif ($ppc != 0) {
+      $sha = 'PPC';
+    } elseif ($trc != 0) {
+      $sha = 'TRC';
+    } elseif ($zet != 0) {
+      $sha = 'ZET';
+    } else {
+      $sha = 'n/a';
+    }
+
+    return $sha;
+  }
+
+  function is_it_scrypt($ltc, $ftc, $mnc, $wdc, $dgc, $nvc, $lky, $arg, $pxc, $mec, $cap, $cgb, $doge, $dmd, $tips, $gdc, $moon) {
+
+    if ($ltc != 0) {
+      $scrypt = 'LTC';
+    } elseif ($ftc != 0) {
+      $scrypt = 'FTC';
+    } elseif ($mnc != 0) {
+      $scrypt = 'MNC';
+    } elseif ($wdc != 0) {
+      $scrypt = 'WDC';
+    } elseif ($dgc != 0) {
+      $scrypt = 'DGC';
+    } elseif ($nvc != 0) {
+      $scrypt = 'NVC';
+    } elseif ($lky != 0) {
+      $scrypt = 'LKY';
+    } elseif ($arg != 0) {
+      $scrypt = 'ARG';
+    } elseif ($pxc != 0) {
+      $scrypt = 'PXC';
+    } elseif ($mec != 0) {
+      $scrypt = 'MEC';
+    } elseif ($cap != 0) {
+      $scrypt = 'CAP';
+    } elseif ($cgb != 0) {
+      $scrypt = 'CGB';
+    } elseif ($doge != 0) {
+      $scrypt = 'DOGE';
+    } elseif ($dmd != 0) {
+      $scrypt = 'DMD';
+    } elseif ($tips != 0) {
+      $scrypt = 'TIPS';
+    } elseif ($gdc != 0) {
+      $scrypt = 'GDC';
+    } elseif ($moon != 0) {
+      $scrypt = 'MOON';
+    } else {
+      $scrypt = 'n/a';
+    }
+
+    return $scrypt;
+
+  }
+
+
+
+  // check if we have something return
+
+  if ($currency) {
+
+    $response['status'] = array(
+      'what' => '200'
+    );
+
+    // do we have a balance
+    foreach ($balances as $currencys => $balance_d) {
+
+      if ($balance_d['confirmed_rewards'] != 0) {
+        $response['currency'][] = array(
+       		'currency' => $currencys,
+          'confirmed_rewards' => $balance_d['confirmed_rewards']
+        );
+      }
+
+    }
+
+    // what are we mining
+    $response['mining'] = array(
+      'scrypt' => is_it_scrypt($currency['ltc']['hashrate'], $currency['ftc']['hashrate'], $currency['mnc']['hashrate'], $currency['wdc']['hashrate'], $currency['dgc']['hashrate'], $currency['nvc']['hashrate'], $currency['lky']['hashrate'], $currency['arg']['hashrate'], $currency['pxc']['hashrate'], $currency['mec']['hashrate'], $currency['cap']['hashrate'], $currency['cgb']['hashrate'], $currency['doge']['hashrate'], $currency['dmd']['hashrate'], $currency['tips']['hashrate'], $currency['gdc']['hashrate'], $currency['moon']['hashrate']),
+      'sha_256' => is_it_sha($currency['btc']['hashrate'], $currency['frc']['hashrate'], $currency['ppc']['hashrate'], $currency['trc']['hashrate'], $currency['zet']['hashrate']),
+    );
+
+
+    //ok lets show the active workers
+
+    foreach ($workers as $worker => $work_d) {
+
+      foreach ($work_d as $work => $hash) {
+        if ($hash['hashrate'] != 0) {
+          $response['workers'][] = array(
+            'coin' => $worker,
+            'worker' => $work,
+            'hashrate' => $hash['hashrate']
+          );
+        }
+      }
+
+    }
+
+  } else {
+
+    $response['status'] = array(
+      'what' => '500'
+    );
+
+  }
+
+
+  echo json_encode($response);
+
+
+?>
--- index.php
+++ index.php
@@ -1,206 +1,205 @@
-<?php 
-	require 'vendor/autoload.php';
+<?php
+  require 'vendor/autoload.php';
 
-	date_default_timezone_set('America/New_York');
+  date_default_timezone_set('America/New_York');
 
-	$app = new \Slim\Slim(array(
+  $app = new \Slim\Slim(array(
     'debug' => true
-	));
+  ));
 
-	$app->setName('multistat-api');
+  $app->setName('multistat-api');
 
-	//Routes
-	$app->get('/', 'home');
+  //Routes
+  $app->get('/', 'home');
 
-	$app->group('/api', function () use ($app) {
+  $app->group('/api', function () use ($app) {
 
-		$app->group('/v1', function () use ($app) {
+    $app->group('/v1', function () use ($app) {
 
-			$app->get('/:api', 'V1:stat');
+      $app->get('/:api', 'V1:stat');
 
-			$app->group('/:api', function () use ($app) {
+      $app->group('/:api', function () use ($app) {
 
-				$app->get('/stat', 'V1:pull');
+        $app->get('/stat', 'V1:pull');
 
-			});
+      });
 
-		});
+    });
 
-	});
+  });
 
-	//$app->contentType('application/json');
+  //$app->contentType('application/json');
 
-	# lets go
-	$app->run();
-
-
-	function home() {
+  # lets go
+  $app->run();
 
 
-	}
+  function home() {
+
+  }
 
 
-	class V1 {
-		
-		#stat
-		public function stat($api) {
-			
-			#connect
-			$es = new Elasticsearch\Client(
-			  array(
-			    'hosts' => array(
-			      '192.241.165.56:9200'
-			    )
-			  )
-			);
+  class V1 {
 
-			$stat = $es->get(
-			  array(
-			  	'index' => 'multitat',
-	        'type'  => 'user',
-			    'id' => $api
-			  )
-			);
+    #stat
+    public function stat($api) {
 
-			echo json_encode($stat);
+      #connect
+      $es = new Elasticsearch\Client(
+        array(
+          'hosts' => array(
+            '192.241.165.56:9200'
+          )
+        )
+      );
 
-		}
+      $stat = $es->get(
+        array(
+        	'index' => 'multitat',
+          'type'  => 'user',
+          'id' => $api
+        )
+      );
 
-		#pull
-		public function pull($api) {
+      echo json_encode($stat);
 
-			include ('lib/functions.php');
+    }
 
-			$url = 'http://api.multipool.us/api.php?api_key='.$api.'';
-			$cmd = array();
-			$data = array('json' => json_encode($cmd));
-			
-			$options = array(
-			  'http' => array(
-			      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-			      'method'  => 'POST',
-			      'content' => http_build_query($data),
-			  ),
-			);
+    #pull
+    public function pull($api) {
 
-			$context  = stream_context_create($options);
-			$result = file_get_contents($url, false, $context);
-			$result = json_decode($result, true);
+      include ('lib/functions.php');
 
-			$currency = $result['currency'];
-			$workers = $result['workers'];
+      $url = 'http://api.multipool.us/api.php?api_key='.$api.'';
+      $cmd = array();
+      $data = array('json' => json_encode($cmd));
 
-			// check if we have something return
-			if ($currency) {
+      $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data),
+        ),
+      );
 
-				foreach ($currency as $currencys => $balance_d) {
-			
-					if ($balance_d['confirmed_rewards'] != 0) {
-						$response['currency'][] = array(
-					 		'currency' => $currencys,
-							'confirmed_rewards' => $balance_d['confirmed_rewards'],
-							'hashrate' => $balance_d['hashrate'],
-							'round_shares' => $balance_d['round_shares'],
-							'block_shares' => $balance_d['block_shares'],
-							'estimated_rewards' => $balance_d['estimated_rewards'],
-							'payout_history' => $balance_d['payout_history'],
-							'pool_hashrate' => $balance_d['pool_hashrate']
-						);
-					}
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+      $result = json_decode($result, true);
 
-				}
+      $currency = $result['currency'];
+      $workers = $result['workers'];
 
-				//ok lets show the active workers
-				foreach ($workers as $worker => $work_d) {
+      // check if we have something return
+      if ($currency) {
 
-					foreach ($work_d as $work => $hash) {
-						if ($hash['hashrate'] != 0) {
-							$response['workers'][] = array(
-								'coin' => $worker,
-								'worker' => $work, 
-								'hashrate' => $hash['hashrate']
-							);
+        foreach ($currency as $currencys => $balance_d) {
 
-							$active[] = array(
-								'coin' => $worker, 
-								'type' => what_is_it($worker)
-							);
-						}
-					}
-					
-				}
+          if ($balance_d['confirmed_rewards'] != 0) {
+            $response['currency'][] = array(
+           		'currency' => $currencys,
+              'confirmed_rewards' => $balance_d['confirmed_rewards'],
+              'hashrate' => $balance_d['hashrate'],
+              'round_shares' => $balance_d['round_shares'],
+              'block_shares' => $balance_d['block_shares'],
+              'estimated_rewards' => $balance_d['estimated_rewards'],
+              'payout_history' => $balance_d['payout_history'],
+              'pool_hashrate' => $balance_d['pool_hashrate']
+            );
+          }
 
-				$active = array_map("unserialize", array_unique(array_map("serialize", $active)));
+        }
 
-				if (false !== ($scrypt = array_search2d('scrypt', $active))) {
-				    $scrypt = $active[$scrypt]['coin'];
-				} else {
-				    $scrypt = 'n/a';
-				}
+        //ok lets show the active workers
+        foreach ($workers as $worker => $work_d) {
 
-				if (false !== ($scrypt_n = array_search2d('scrypt-n', $active))) {
-				    $scrypt_n = $active[$scrypt_n]['coin'];
-				} else {
-				    $scrypt_n = 'n/a';
-				}
+          foreach ($work_d as $work => $hash) {
+            if ($hash['hashrate'] != 0) {
+              $response['workers'][] = array(
+                'coin' => $worker,
+                'worker' => $work,
+                'hashrate' => $hash['hashrate']
+              );
 
-				if (false !== ($sha_256 = array_search2d('sha-256', $active))) {
-				    $sha_256 = $active[$sha_256]['coin'];
-				} else {
-				    $sha_256 = 'n/a';
-				}
+              $active[] = array(
+                'coin' => $worker,
+                'type' => what_is_it($worker)
+              );
+            }
+          }
 
-				if (false !== ($x11 = array_search2d('x11', $active))) {
-				    $x11 = $active[$x11]['coin'];
-				} else {
-				    $x11 = 'n/a';
-				}
+        }
 
-				$response['mining'] = array(
-					'scrypt' => $scrypt,
-					'scrypt_n' => $scrypt_n,
-					'sha_256' => $sha_256,
-					'x11' => $x11
-				);
+        $active = array_map("unserialize", array_unique(array_map("serialize", $active)));
 
-				$url = 'https://multistat.firebaseio.com/users/'.$api.'.json';
-		 
-				$content = json_encode($response);
+        if (false !== ($scrypt = array_search2d('scrypt', $active))) {
+            $scrypt = $active[$scrypt]['coin'];
+        } else {
+            $scrypt = 'n/a';
+        }
 
-				$curl = curl_init($url);
-				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-				curl_setopt($curl, CURLOPT_HEADER, false);
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-				curl_setopt($curl, CURLOPT_POST, true);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        if (false !== ($scrypt_n = array_search2d('scrypt-n', $active))) {
+            $scrypt_n = $active[$scrypt_n]['coin'];
+        } else {
+            $scrypt_n = 'n/a';
+        }
 
-				$json_response = curl_exec($curl);
+        if (false !== ($sha_256 = array_search2d('sha-256', $active))) {
+            $sha_256 = $active[$sha_256]['coin'];
+        } else {
+            $sha_256 = 'n/a';
+        }
 
-				$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (false !== ($x11 = array_search2d('x11', $active))) {
+            $x11 = $active[$x11]['coin'];
+        } else {
+            $x11 = 'n/a';
+        }
 
-				curl_close($curl);
+        $response['mining'] = array(
+          'scrypt' => $scrypt,
+          'scrypt_n' => $scrypt_n,
+          'sha_256' => $sha_256,
+          'x11' => $x11
+        );
 
-				$response = json_decode($json_response, true);
+        $url = 'https://multistat.firebaseio.com/users/'.$api.'.json';
 
-				
-				$call_response['response'] = array(
-					'status' => '200'
-				);
+        $content = json_encode($response);
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+        $json_response = curl_exec($curl);
+
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        curl_close($curl);
+
+        $response = json_decode($json_response, true);
 
 
-			} else { // panic
+        $call_response['response'] = array(
+          'status' => '200'
+        );
 
-				$call_response['response'] = array(
-					'status' => '500'
-				);
 
-			}
+      } else { // panic
 
-			echo json_encode($call_response);
+        $call_response['response'] = array(
+          'status' => '500'
+        );
 
-		}
+      }
 
-	}
-	
+      echo json_encode($call_response);
+
+    }
+
+  }
+
 ?>
